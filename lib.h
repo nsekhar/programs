a73a2327550a9588889b89673966ae0afc6e96c2
Sekhar Nori <nsekhar@ti.com>
2014-02-10 15:04:20 +0530

    lib.h: add a macro to print error and return

--- lib.h
+++ lib.h
@@ -1,2 +1,19 @@
+#ifndef _LIB_H
+#define _LIB_H
+
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+
+
 void swap(int *a, int *b);
 void printarray(int a[], int n);
+
+#define PERROR_RET(fmt, arg...)	do {				\
+		int errsv = errno;				\
+		printf(fmt ": %s\n", ##arg, strerror(errsv));	\
+		return errsv;					\
+	} while (0);
+
+#endif
