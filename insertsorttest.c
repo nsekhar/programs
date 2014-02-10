2b17ec4c34bb372de99cb83f01963739a25e2cd3
Sekhar Nori <nsekhar@ti.com>
2014-02-10 15:05:20 +0530

    insertsort: add ability to read from file

--- insertsorttest.c
+++ insertsorttest.c
@@ -1,12 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "lib.h"
 #include "insertsort.h"
 
+#define MAX_NUM	10000
+
 int main()
 {
-	int a[] = {2, 5, 1, 3, 10};
-	insertsort(a, sizeof(a)/sizeof(int));
-	printarray(a, sizeof(a)/sizeof(int));
+	FILE *f;
+	int *a, i = 0;
+
+	f = fopen("data", "r");
+	if (!f)
+		PERROR_RET("Unable to open file data for reading");
+
+
+	a = (int *) calloc(sizeof(int), MAX_NUM);
+
+	while (!feof(f)) {
+		fscanf(f, "%d", &a[i]);
+		i++;
+	}
+
+	insertsort(a, i - 1);
+	printarray(a, i - 1);
+
+	free(a);
 
 	return 0;
 }
