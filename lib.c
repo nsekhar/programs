1cd0ae1467cd6f52f41eae58c1ece5bf223e83f7
Sekhar Nori <nsekhar@ti.com>
2014-02-13 18:13:11 +0530

    lib: add support to timestamp code

--- lib.c
+++ lib.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+
+#include <sys/time.h>
+
 #include "lib.h"
+
+__thread struct timeval tv_start, tv_stop;
 
 void swap(int *a, int *b)
 {
@@ -17,4 +22,19 @@ void printarray(int a[], int n)
 		printf("%d ", a[i]);
 
 	printf("\n");
+}
+
+void start_timestamp()
+{
+	gettimeofday(&tv_start, NULL);
+}
+
+time_t stop_timestamp()
+{
+	struct timeval tv_res;
+
+	gettimeofday(&tv_stop, NULL);
+	timersub(&tv_stop, &tv_start, &tv_res);
+
+	return tv_res.tv_sec * 1000000UL + tv_res.tv_usec;
 }
--- lib.h
+++ lib.h
@@ -5,10 +5,12 @@
 #include <errno.h>
 #include <string.h>
 
-
+#include <sys/time.h>
 
 void swap(int *a, int *b);
 void printarray(int a[], int n);
+void start_timestamp();
+time_t stop_timestamp();
 
 #define PERROR_RET(fmt, arg...)	do {				\
 		int errsv = errno;				\
